6369e6d96fc6149de1834455d33a8c720f0705bd
Danyon Ramsay <danyon.ramsay@yahoo.com.au>
2017-05-03 10:04:18 +1000

    Added debugging output to index.php

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
 
+    echo "started file";
+
     session_start();
 
+    echo "started session";
+
     include_once $_SERVER['DOCUMENT_ROOT'] . "/lib/helper.php";
+
+    echo "included";
 
     if (Helper::isLoggedIn()) {
         Helper::redirect("/home.php");
